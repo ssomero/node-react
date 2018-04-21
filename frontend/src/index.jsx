5dec2a838ed9aebf799415303511cbb93a808e71
Sonja Somero <sonja.somero@helsinki.fi>
2018-04-21 21:20:18 +0300

    More debugging to frontend

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,8 +5,10 @@ import ReactDOM from 'react-dom';
 import './assets/stylesheets/style.css';
 import config from './config';
 
-const { baseURL } = config[process.env.NODE_ENV];
-
+console.log(config);
+console.log(process.env);
+const env = process.env.NODE_ENV;
+const baseURL = config[env].baseURL;
 const getGreetings = async () => {
   console.log('baseUrl');
   console.log(baseURL);
